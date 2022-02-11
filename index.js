b45a546c056cb9919439b3a01e84bd196eb5a092
ValterSMachadojr <machatec41@gmail.com>
2022-02-11 15:04:45 -0300

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require ("express");
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
+
+
 const produto = require("./rotas/produto");
 const cor = require("./rotas/cor")
 const marca = require("./rotas/marca")
